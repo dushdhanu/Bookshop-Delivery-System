33868151e4441d9a9f2ccf716f3f3c0c3d885c31
dushdhanu <dushmanthidananjana4@gmail.com>
2025-12-05 12:48:29 +0530

    Upload the main structure of the Bookshop system

---
+++ Bookshop_Delivery_system/src/main/java/ac/nsbm/bookshop_delivery_system/BookshopDeliverySystemApplication.java
@@ -0,0 +1,13 @@
+package ac.nsbm.bookshop_delivery_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopDeliverySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopDeliverySystemApplication.class, args);
+    }
+
+}
---
+++ Bookshop_Delivery_system/src/test/java/ac/nsbm/bookshop_delivery_system/BookshopDeliverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.nsbm.bookshop_delivery_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopDeliverySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
